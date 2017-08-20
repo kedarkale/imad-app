0401ea396f64914c3b54c56d81ac4f23f1e818d5
Kedar K. Kale <kedar30092010@gmail.com>
2017-08-20 13:42:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 var request = new XMLHttpRequest();
-request.open("GET","http://kedarcoc.imad.hasura-app.io/visits");
+request.open("GET","http://kedarcoc.imad.hasura-app.io/visits",true);
 var count=request.responseText;
 var span =document.getElementById('visits');
 span.innerHTML=count;
