8c0a1225f8d496c38719a648afbf7c4f22f3a6de
Kedar K. Kale <kedar30092010@gmail.com>
2017-08-25 15:40:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ app.get('/:article_name', function (req, res) {
        if(error){
            res.status(500).send(error.toString());
        }else{
-           if (res.rows.length===0){
+           if (result.rows.length===0){
                res.status(404).send('Article not found');
            }else{
                var articleData = result.rows[0];
